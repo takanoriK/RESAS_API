57f3a8caf38c06ebdbf02b8c5ce18f4b190c89f7
takanori <kyukitakanori@hisakitakashihan-no-MacBook.local>
2018-09-30 20:44:07 +0900

    画像取得完成

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,28 +1,24 @@
 class TopController < ApplicationController
   protect_from_forgery except: :get_data
-  
-  @@api =UseApiController.new()
+
+  @@api = UseApiController.new()
 
   def index
       # 都道府県のID, 名称を取得
-      pref_data = commonPrefecturesData()
+      pref_data = @@api.getPrefecturesData()
       @select_box_pref_data = pref_data['result']
-
   end
 
   def get
       pref_name = params['pref_name']
       @pref_id = params[:prefecture]
-
-      @data = commonPrefecturesData()
+      
+      #DBから取得したデータをBingSearchApiに渡す
+      term = "マキシマムザホルモン"
+      image_data = @@api.getBingSearchImageData(term)
+      @images = image_data['value']
       render "get"
   end
 
-
-  private
-      def commonPrefecturesData()
-          prefectures_data = @@api.getPrefecturesData("https://opendata.resas-portal.go.jp/api/v1-rc.1/prefectures")
-
-          return prefectures_data
-      end
 end
+
--- app/controllers/use_api_controller.rb
+++ app/controllers/use_api_controller.rb
@@ -3,19 +3,43 @@ require 'uri'
 require 'json'
 
 class UseApiController < ApplicationController
-    def getPrefecturesData(url)
-        uri = URI.parse(URI.escape(url))
+    def getPrefecturesData()
+        uri = URI.parse(URI.escape("https://opendata.resas-portal.go.jp/api/v1-rc.1/prefectures"))
 
         https = Net::HTTP.new(uri.host, uri.port)
         https.use_ssl = true
 
         param = {}
-        param['X-API-KEY'] = ''
+        param['X-API-KEY'] = 'ぱすわーど'
 
         req = Net::HTTP::Get.new(uri.request_uri, param)
         res = https.request(req)
 
         json = res.body
         result = JSON.parse(json)
+    end
+
+    def getBingSearchImageData(term)
+        accessKey = "ぱすわーど"
+        uri       = "https://api.cognitive.microsoft.com"
+        path      = "/bing/v7.0/images/search"
+
+        if accessKey.length != 32 then
+            puts "Invalid Bing Search API subscription key!"
+            puts "Please paste yours into the source code."
+            abort
+        end
+
+        uri = URI(uri + path + "?q=" + URI.escape(term))
+        
+        request = Net::HTTP::Get.new(uri)
+        request['Ocp-Apim-Subscription-Key'] = accessKey
+        
+        response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+            http.request(request)
+        end
+        
+        result = JSON.parse(response.body)
+        
     end
 end
